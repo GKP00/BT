2881319e96ef985194e0725d6f29e87b5b984200
GKPedrsen <GKPedrsen@gmail.com>
2025-05-24 14:41:12 +0200

    BEncoding: add BDict + refactoring

--- include/BEncoding.hpp
+++ include/BEncoding.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
 #include <charconv>
-#include <istream>
+#include <iostream>
+#include <map>
 #include <vector>
 #include <variant>
 
@@ -11,32 +12,57 @@ namespace BT::BEncoding
 using BInt = int;
 using BStr = std::string;
 
-struct BElement;
-using BList = std::vector<BElement>;
-
-struct BElement : public std::variant<BInt, BStr, BList>
-{
-  using std::variant<BInt, BStr, BList>::variant;
-};
+struct BElem;
+using BList = std::vector<BElem>;
+using BDict = std::map<BStr, BElem>;
 
 enum class Type
 {
+  None = 0,
   BInt,
   BStr,
   BList,
   BDict,
-  INVALID,
+};
+
+std::ostream& operator<<(std::ostream& stream, Type type)
+{
+  switch(type)
+  {
+    case Type::BInt:  stream << "BInt";  break;
+    case Type::BStr:  stream << "BStr";  break;
+    case Type::BList: stream << "BList"; break;
+    case Type::BDict: stream << "BDict"; break;
+    case Type::None:  stream << "None";  break;
+  }
+
+  return stream;
+}
+
+using BElemVariant = std::variant<std::monostate, BInt, BStr, BList, BDict>;
+struct BElem : public BElemVariant
+{
+  using BElemVariant::variant;
+
+  Type GetType() const
+  {
+    return std::holds_alternative<BInt>(*this)  ? Type::BInt  :
+           std::holds_alternative<BStr>(*this)  ? Type::BStr  :
+           std::holds_alternative<BList>(*this) ? Type::BList : 
+           std::holds_alternative<BDict>(*this) ? Type::BDict : Type::None;
+  }
 };
 
 Type PeekNext(std::istream& stream)
 {
-  char next = stream.peek();
+  int next = stream.peek();
   return next == 'i' ? Type::BInt  :
          next == 'l' ? Type::BList :
          next == 'd' ? Type::BDict :
-         std::isdigit(next) && next != '0' ? Type::BStr :
-         Type::INVALID;
+         next >= '0' && next <= '9' ? Type::BStr : Type::None;
 }
+
+BElem Parse(std::istream& stream);
 
 BInt ParseBInt(std::istream& stream)
 {
@@ -109,9 +135,8 @@ BStr ParseBStr(std::istream& stream)
     throw std::runtime_error{"invalid bstr: std::from_chars failed"};
 
   //read string off stream
-  std::string str;
-  while(len--)
-    str += stream.get();
+  std::string str(len, '\0');
+  stream.read(&str[0], len);
 
   return str;
 }
@@ -125,26 +150,117 @@ BList ParseBList(std::istream& stream)
   //consume start idenfitier
   stream.get();
 
-  std::vector<BElement> list;
+  std::vector<BElem> list;
 
   //parse list elements until end idenfitier 'e' is encountered
   while(stream.peek() != 'e')
-  {
-    switch(PeekNext(stream))
-    {
-      case Type::BStr: list.emplace_back( ParseBStr(stream) ); break;
-      case Type::BInt: list.emplace_back( ParseBInt(stream) ); break;
-      case Type::BList: list.emplace_back( ParseBList(stream) ); break;
+    list.emplace_back( BEncoding::Parse(stream) );
 
-      case Type::BDict:
-      case Type::INVALID:
-        throw std::runtime_error{"invalid blist: invalid or unimplemented element encountered"};
-
-    }
-
-  }
+  //consume end identifier
   stream.get();
+
   return list;
 }
+
+BDict ParseBDict(std::istream& stream)
+{
+  //check for start identifier char 'd'
+  if(stream.peek() != 'd')
+    throw std::runtime_error{"invalid bdict: no start identifier"};
+
+  //consume start idenfitier
+  stream.get();
+
+  BDict dict;
+
+  //ensure next element is a string
+  if(PeekNext(stream) != Type::BStr)
+    throw std::runtime_error{"invalid bdict: key is not string"};
+
+  //parse key value pairs until end idenfitier 'e' is encountered
+  while(stream.peek() != 'e')
+  {
+    BStr  key = ParseBStr(stream);
+    BElem val = BEncoding::Parse(stream);
+    dict[key] = val;
+  }
+  
+  //consume end identifier
+  stream.get();
+
+  return dict;
+}
+
+BElem Parse(std::istream& stream)
+{
+  switch( PeekNext(stream) )
+  {
+    case Type::BInt:  return ParseBInt(stream);
+    case Type::BStr:  return ParseBStr(stream);
+    case Type::BList: return ParseBList(stream);
+    case Type::BDict: return ParseBDict(stream);
+  }
+
+  return std::monostate{};
+}
+
+std::istream& operator>>(std::istream& stream, BElem& elem)
+{
+  elem = Parse(stream);
+
+  if(elem.GetType() == Type::None)
+    stream.setstate(std::ios::failbit);
+
+  return stream;
+}
+
+void Serialize(std::ostream&, const BElem&);
+
+void SerializeBInt(std::ostream& stream, const BInt& bint)
+{
+  stream << 'i' << bint << 'e';
+}
+
+void SerializeBStr(std::ostream& stream, const BStr& bstr)
+{
+  stream << bstr.length() << ':' << bstr;
+}
+
+void SerializeBList(std::ostream& stream, const BList& blist)
+{
+  stream << 'l';
+  for(const BElem& elem : blist)
+    Serialize(stream, elem);
+  stream << 'e';
+}
+
+void SerializeBDict(std::ostream& stream, const BDict& bdict)
+{
+  stream << 'd';
+  for(const auto& kv : bdict)
+  {
+    SerializeBStr(stream, kv.first);
+    Serialize(stream, kv.second);
+  }
+  stream << 'e';
+}
+
+void Serialize(std::ostream& stream, const BElem& elem)
+{
+  switch( elem.GetType() )
+  {
+    case Type::BInt:  SerializeBInt(stream,  std::get<BInt>(elem));  break;
+    case Type::BStr:  SerializeBStr(stream,  std::get<BStr>(elem));  break;
+    case Type::BList: SerializeBList(stream, std::get<BList>(elem)); break;
+    case Type::BDict: SerializeBDict(stream, std::get<BDict>(elem)); break;
+  }
+}
+
+std::ostream& operator<<(std::ostream& stream, const BElem& elem)
+{
+  Serialize(stream, elem);
+  return stream;
+}
+
 
 } //namespace: BT::BEncoding
